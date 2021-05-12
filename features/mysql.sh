31fff4b6066f4aa7e6369661c267f643d274cb09
Cyril Wanner <info@cyr.li>
2021-05-12 20:40:01 +0200

    Fix mysql installation

--- features/mysql.sh
+++ features/mysql.sh
@@ -7,6 +7,7 @@ apt-get install --yes mysql-server
 mysql --user=root <<EOF
   DELETE FROM mysql.user WHERE User='';
   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
   DROP DATABASE IF EXISTS test;
   DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
   FLUSH PRIVILEGES;
