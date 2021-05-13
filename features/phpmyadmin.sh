8f0bfbfb47e43cc20b577ad1d61d7fd009bb848d
Cyril Wanner <info@cyr.li>
2021-05-13 12:32:45 +0200

    Update phpmyadmin version

--- features/phpmyadmin.sh
+++ features/phpmyadmin.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # install phpmyadmin
+add-apt-repository --yes ppa:phpmyadmin/ppa
+apt-get update
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
 DEBIAN_FRONTEND=noninteractive apt-get install --yes phpmyadmin
 
 # configure phpmyadmin
 chown $SUDO_UID:$SUDO_GID /var/lib/phpmyadmin/tmp
-cp "$SETUP_DIR/configs/phpmyadmin/config.trade17.php" /etc/phpmyadmin/conf.d/config.trade17.php
-chown $SUDO_UID:$SUDO_GID /etc/phpmyadmin/conf.d/config.trade17.php
+cp "$SETUP_DIR/configs/phpmyadmin/config.skills17.php" /etc/phpmyadmin/conf.d/config.skills17.php
+chown $SUDO_UID:$SUDO_GID /etc/phpmyadmin/conf.d/config.skills17.php
