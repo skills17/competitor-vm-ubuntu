43210897652d43fcc116445915755b293ad2472e
Cyril Wanner <info@cyr.li>
2021-12-26 20:49:18 +0100

    Update eclipse

--- features/eclipse.sh
+++ features/eclipse.sh
@@ -4,7 +4,7 @@
 apt-get install --yes openjdk-16-jre-headless
 
 # install eclipse (from official mirror)
-curl -L https://rhlx01.hs-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2021-03/R/eclipse-php-2021-03-R-linux-gtk-x86_64.tar.gz -o /tmp/eclipse.tar.gz
+curl -L https://rhlx01.hs-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2021-12/R/eclipse-php-2021-12-R-linux-gtk-x86_64.tar.gz -o /tmp/eclipse.tar.gz
 tar -xzf /tmp/eclipse.tar.gz -C /opt
 rm /tmp/eclipse.tar.gz
 
