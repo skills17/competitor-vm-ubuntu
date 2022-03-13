500d28cfad84b47ce0b4e4816e91375132f6e205
Cyril Wanner <info@cyr.li>
2022-03-13 14:08:11 +0100

    Add cypress dependencies

---
+++ features/cypress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install cypress dependencies
+apt-get install --no-install-recommends --yes \
+    libgtk2.0-0 \
+    libgtk-3-0 \
+    libgbm-dev \
+    libnotify-dev \
+    libgconf-2-4 \
+    libnss3 \
+    libxss1 \
+    libasound2 \
+    libxtst6 \
+    xauth \
+    xvfb
