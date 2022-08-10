c628b6b45500bb8b64bb507036a1627c495af6ff
Cyril Wanner <info@cyr.li>
2022-08-10 10:19:28 +0200

    Update python setup for Ubuntu 22.04

--- features/sublime.sh
+++ features/sublime.sh
@@ -5,7 +5,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 apt-get install --yes apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 apt-get update
-apt-get install --yes sublime-text python python3-pip
+apt-get install --yes sublime-text python-is-python3 python3-pip
 
 # install extensions
 if [[ ! -d "$HOME/.config/sublime-text-3/Packages/User/" ]]; then
