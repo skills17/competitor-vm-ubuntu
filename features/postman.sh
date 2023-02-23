c293273d2a4cf95099fd0005316fbc65f3265a73
Cyril Wanner <info@cyr.li>
2023-02-23 22:43:11 +0100

    Install postman on ARM builds

--- features/postman.sh
+++ features/postman.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 # install postman
-if [[ "$(dpkg --print-architecture)" = arm* ]]; then
-  echo "Postman is still working on ARM support, see the ARM readme of competitor-vm-ubuntu for more information"
-  sleep 10
-else
-  snap install postman
-fi
+snap install postman
