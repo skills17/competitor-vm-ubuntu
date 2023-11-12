3fe0fc2beb562b484ceaf3aedc37eedf7d688bb1
Cyril Wanner <info@cyr.li>
2023-11-12 15:49:05 +0100

    Update nodejs installation script

--- features/nodejs.sh
+++ features/nodejs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # install nodejs
-curl -L https://deb.nodesource.com/setup_18.x | bash -
+apt-get update
+apt-get install --no-install-recommends --yes ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
 apt-get install --yes nodejs
 
 # install global packages
@@ -10,3 +15,4 @@ npm install --global eslint
 # ensure correct permissions
 mkdir -p "$HOME/.config/configstore"
 chown -R $SUDO_UID:$SUDO_GID "$HOME/.config/configstore"
+chown -R $SUDO_UID:$SUDO_GID "$HOME/.npm"
